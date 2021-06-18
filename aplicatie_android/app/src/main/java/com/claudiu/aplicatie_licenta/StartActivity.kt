9c6ae5e50796d557ac6ae9add192059aa6736f76
ClaudiuFilip110 <clawdelu.filip@gmail.com>
2021-06-18 15:50:02 +0300

    add visual elements

--- aplicatie_android/app/src/main/java/com/claudiu/aplicatie_licenta/StartActivity.kt
+++ aplicatie_android/app/src/main/java/com/claudiu/aplicatie_licenta/StartActivity.kt
@@ -22,6 +22,7 @@ class StartActivity : AppCompatActivity() {
         Manifest.permission.READ_EXTERNAL_STORAGE,
         Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
+    private lateinit var text: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,65 +48,87 @@ class StartActivity : AppCompatActivity() {
         }
     }
 
+    fun readFromFile(file: File): Array<Array<FloatArray>>{
+
+        val input: InputStream
+
+        input = FileInputStream(file)
+
+        val a = Array(1) {
+            Array(16) {
+                FloatArray(9)
+            }
+        }
+
+        val reader = BufferedReader(InputStreamReader(input))
+        for (row in a[0].indices) {
+            val linee: String = reader.readLine()
+            val lineElems = linee.split(" ")
+            for (it in a[0][row].indices) {
+                a[0][row][it] = lineElems[it].toFloat()
+            }
+        }
+
+        input.close()
+        return a
+    }
+
     fun model() {
         if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             val sdcard = this.filesDir
             val file = File("$sdcard", "INFERENCE_DATA")
 
-            val input: InputStream
 
-            input = FileInputStream(file)
-
-
-            val a = Array(1) {
-                Array(16) {
-                    FloatArray(9)
-                }
-            }
-
-            val reader = BufferedReader(InputStreamReader(input))
-            for (row in a[0].indices) {
-                val linee: String = reader.readLine()
-                val lineElems = linee.split(" ")
-                for (it in a[0][row].indices) {
-                    a[0][row][it] = lineElems[it].toFloat()
-                }
-            }
-
+            /** deprecated version of code
             var byteBuffer : ByteBuffer =  ByteBuffer.allocate(1*16*9*4)
             for (row in a[0].indices) {
                 for (it in a[0][row].indices) {
                     byteBuffer.putFloat(a[0][row][it])
                 }
             }
+             ....
 
-            var floatVector = floatArrayOf()
-            Log.d("VECTOR",floatVector.contentToString())
-            for (row in a[0].indices) {
-                for (it in a[0][row].indices) {
-                    floatVector = floatVector.plus(a[0][row][it])
-                }
-            }
-            Log.d("VECTOR",floatVector.contentToString())
+            inputFeature0.loadBuffer(byteBuffer)
+            */
 
-            val model = ModelAndroid.newInstance(this)
-            
+            val a = readFromFile(file)
+            var floatVector = shrinkShapeForModel(a)
+            val probabilities = modelInference(floatVector)
 
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 16, 9), DataType.FLOAT32)
-//            inputFeature0.loadBuffer(byteBuffer)
-            inputFeature0.loadArray(floatVector)
+            val output = determineOutputFromProbabilites(probabilities)
 
-
-            val outputs = model.process(inputFeature0)
-            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-
-            for (FLOAT in outputFeature0.floatArray) {
-                Log.d("OUTPUT", "${FLOAT}")
-            }
-
-            input.close()
-            model.close()
-
+            text = "The price of the certain coin is predicted to be $output than before"
         }
+    }
+
+    private fun determineOutputFromProbabilites(probabilities: FloatArray): String {
+        val maxIdx = probabilities.indices.maxBy { probabilities[it] } ?: -1
+        return if(maxIdx == 1)
+            "higher"
+        else
+            "lower"
+    }
+
+    private fun modelInference(floatVector: FloatArray): FloatArray {
+        val model = ModelAndroid.newInstance(this)
+
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 16, 9), DataType.FLOAT32)
+        inputFeature0.loadArray(floatVector)
+
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        model.close()
+        return outputFeature0.floatArray
+    }
+
+    private fun shrinkShapeForModel(a: Array<Array<FloatArray>>): FloatArray {
+        var floatVector = floatArrayOf()
+        for (row in a[0].indices) {
+            for (it in a[0][row].indices) {
+                floatVector = floatVector.plus(a[0][row][it])
+            }
+        }
+        return floatVector
     }
 }
